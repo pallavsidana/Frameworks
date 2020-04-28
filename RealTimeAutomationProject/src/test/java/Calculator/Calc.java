1fdcaf127f8892ea4ce227b0f41a76c36116a853
pallavsidana <44015158+pallavsidana@users.noreply.github.com>
2020-04-28 15:10:07 +0530

    Updated the function as per the new requirement

--- RealTimeAutomationProject/src/test/java/Calculator/Calc.java
+++ RealTimeAutomationProject/src/test/java/Calculator/Calc.java
@@ -3,7 +3,7 @@ package Calculator;
 import org.testng.annotations.Test;
 
 public class Calc {
-	@Test
+	@Test(description="Addition")
 	public void add() {
 		int a=10;
 		int b=90;
@@ -11,30 +11,30 @@ public class Calc {
 		System.out.println("addition value of c is ::::"+ c);
 	}
 	
-	@Test
+	@Test(description="Substraction")
 	public void Sub() {
-		int a=1000;
+		int a=100;
 		int b=90;
 		int c= a-b;
 		System.out.println("subtrction value of c is ::::"+ c);
 	}
-	@Test
+	@Test(description="Multiplication")
 	public void Mul() {
-		int a=1000;
+		int a=12000;
 		int b=90;
 		int c= a*b;
 		System.out.println("multiply value of c is ::::"+ c);
 	}
-	@Test
+	@Test(description="Division")
 	public void DIV() {
-		int a=1000;
+		int a=7000;
 		int b=90;
 		int c= a/b;
 		System.out.println("Division value of c is ::::"+ c);
 	}
-	@Test
+	@Test(description="Precentage")
 	public void Percentage() {
-		int a=1000;
+		int a=5000;
 		int b=90;
 		int c= (a*b)/100;
 		System.out.println("Percentage value of c is ::::"+ c);
